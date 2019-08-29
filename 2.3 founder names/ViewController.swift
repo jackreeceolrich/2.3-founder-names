0baa6a42114b04a6dc3d15eb1c8ddda0bb10213c
Reec Olrich <jolrich@zagmail.gonzaga.edu>
2019-08-29 15:16:59 -0700

    Initial Commit

---
+++ 2.3 founder names/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2.3 founder names
+//
+//  Created by reece olrich on 8/29/19.
+//  Copyright © 2019 reece olrich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
